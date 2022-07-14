ba9323d1f7fc798cc09aa3ef0bfc46488d6e577b
Brad Teague <b_rad88@live.com>
2022-07-14 20:55:33 +0930

    Feature/build request based on meal type

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const recipesContainer = document.getElementById("recipes-container");
 const getRecipesBtn = document.getElementById("get-recipes"); // as HTMLButtonElement;
 const queryInput = document.getElementById("query-input"); // as HTMLInputElement;
+const mealTypeButtons = document.getElementById("meal-type"); // as HTMLSectionElement
 
 const apiId = "bcfab256";
 const apiKey = "f61e0b4ed3f4b5d198cb292a5bf83160";
+
+let options = {}; // as QueryOptions;
+
+mealTypeButtons.addEventListener("click", e => {
+  if (!e.target.matches("button")) return;
+  const { mealtype } = e.target.dataset;
+  appendMealType(mealtype);
+});
 
 const url = new URL("api/recipes/v2", "https://api.edamam.com");
 url.searchParams.append("app_id", apiId);
 url.searchParams.append("app_key", apiKey);
 url.searchParams.append("type", "public");
 
-getRecipesBtn.addEventListener("click", () => {
+// interface QueryOptions {
+//   mealType?: string;
+// }
+
+getRecipesBtn.addEventListener("click", e => {
   const query = queryInput.value;
   fetchData(query);
 });
@@ -32,15 +45,26 @@ getRecipesBtn.addEventListener("click", () => {
 //   hits: Recipe[];
 // }
 
-async function fetchData(query, options = {}) {
+async function fetchData(query) {
   url.searchParams.append("q", query);
+  appendOptions(url, options);
   const request = new Request(url);
-  const response = await fetch(request);
-  if (!response.ok) return;
-  const data = await response.json();
-  console.log(data);
+  console.log(request);
+  // const response = await fetch(request);
+  // if (!response.ok) return;
+  // const data = await response.json();
+  // console.log(data);
 }
 
 // function renderRecipes(data) {}
 
-// function appendOptions(url, options) {}
+function appendOptions(url, options) {
+  Object.entries(options).forEach(([key, val]) => {
+    url.searchParams.append(key, val);
+  });
+  // url.searchParams.append();
+}
+
+function appendMealType(mealType) {
+  options.mealType = mealType;
+}
